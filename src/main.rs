7ea934d7906a50fb0dbe2167282a3c113c3e14ac
Giorgos Karagounis <giorgos.karagounis@oqton.com>
2022-01-08 17:28:06 +0100

    switch std::env::args to structopt crate; add dead eyes option

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
+use structopt::StructOpt;
+/// Let the cat speak for you in the shell
+#[derive(StructOpt, Debug)]
+#[structopt(name = "catsay")]
+struct CatsayArgs {
+    /// The message the cat should be saying
+    #[structopt(short, long, default_value="Hello, world!")]
+    message: String,
+    /// Show the cat to be dead
+    #[structopt(short, long)]
+    dead: bool,
+}
 fn main() {
-    let message = std::env::args().nth(1).expect("Usage: catsay \"[message]\"");
-    println!("{}", message);
+    let opt = CatsayArgs::from_args();
+    println!("{:?}", opt);
+    let eye = match opt.dead {
+        true => 'x',
+        false => '°'
+    };
+    println!("{}", opt.message);
     println!(" \\");
     println!("  \\");
     println!("    ^_^");
-    println!("  ( ° ° )");
+    println!("  ( {} {} )", eye, eye);
     println!("   =(I)=");
 }
