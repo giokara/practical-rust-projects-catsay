4d3aa4cd7ff91dd6ece204f28e58e8f858dab8da
Giorgos Karagounis <giorgos.karagounis@oqton.com>
2022-01-09 14:14:36 +0100

    allow to define cat string from file

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,30 @@ struct CatsayArgs {
     /// Show the cat to be dead
     #[structopt(short, long)]
     dead: bool,
+    /// Read the cat figure from a file
+    #[structopt(short, long, parse(from_os_str))]
+    file: Option<std::path::PathBuf>,
 }
 fn main() {
     let opt = CatsayArgs::from_args();
-    println!("{:?}", opt);
+    if opt.message.to_lowercase().contains("woof") {
+        eprintln!("A cat shouldn't bark!");
+    }
+    let mut cat = concat!(
+        " \\\n",
+        "  \\\n",
+        "    ^_^\n",
+        "  ( {eye} {eye} )\n",
+        "   =(I)="
+    ).to_string();
     let eye = match opt.dead {
         true => "x".red().bold(),
         false => "°".normal()
     };
-    if opt.message.to_lowercase().contains("woof") {
-        eprintln!("A cat shouldn't bark!");
+    if opt.file.is_some() {
+        cat = std::fs::read_to_string(opt.file.unwrap()).expect("Could not parse file");
     }
     println!("{}", opt.message.yellow().on_purple());
-    println!(" \\");
-    println!("  \\");
-    println!("    ^_^");
-    println!("  ( {} {} )", eye, eye);
-    println!("   =(I)=");
+    // TODO fix dead eye formatting
+    println!("{}", cat.replace("{eye}", &eye));
 }
